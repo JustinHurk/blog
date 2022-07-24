03c9599ddefbb7c75a371a57931c9c6f4d85f5a3
JustinHurk <108637175+JustinHurk@users.noreply.github.com>
2022-07-25 08:50:07 +0930

    commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>My Blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni rem facere necessitatibus quam nulla, molestias ad quae, eaque voluptates tempore saepe quibusdam soluta, laborum tenetur voluptatem. Facilis consequuntur cum vero?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni rem facere necessitatibus quam nulla, molestias ad quae, eaque voluptates tempore saepe quibusdam soluta, laborum tenetur voluptatem. Facilis consequuntur cum vero?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni rem facere necessitatibus quam nulla, molestias ad quae, eaque voluptates tempore saepe quibusdam soluta, laborum tenetur voluptatem. Facilis consequuntur cum vero?
+        </p>
+    </article>
+</body>
+
+</html>
